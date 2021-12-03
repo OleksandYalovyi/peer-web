541c1d085443e7360055e622acd35605f37867e2
Alexey Lazarev <chisw.dnepr@gmail.com>
2021-12-03 09:42:13 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MainContainer from 'components/MainContainer'
+
+import './App.css'
+
+function App() {
+  return (
+    <MainContainer>
+      <h1>Peer.inc</h1>
+    </MainContainer>
+  )
+}
+
+export default App
