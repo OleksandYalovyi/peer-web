7c04116b3ab19dc7226e51f804e3eb2e6e10e6db
Andrii Zakharchenko <art.pruttov@gmail.com>
2022-07-27 10:57:21 +0300

    feature: add footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,39 @@
 import React from 'react'
-
 import PropTypes from 'prop-types'
 import styles from './styles.module.scss'
 
-const Footer = () => <div className={styles.container}>footer</div>
+const Footer = () => (
+  <div className={styles.footer}>
+    <div className={styles.container}>
+      <div className={styles.divider} />
+      <div className={styles.content_section}>
+        <div className={styles.column_left}>
+          <div className={styles.content_wrapper}>
+            <p>
+              Peer is a metaverse company based in Seattle. We’re focused on developing consumer
+              electronics, software, and blockchain services for the future of computing.
+            </p>
+            <p>Our mission is to bring the world into the metaverse.</p>
+          </div>
+        </div>
+        <div className={styles.column_right}>
+          <div className={styles.block}>
+            <div className={styles.title}>Into the metaverse.</div>
+            <div className={styles.text}>The future never stop coming.</div>
+          </div>
+          <div className={styles.block}>
+            <div className={styles.title}>Sign up for product updates</div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.divider} />
+      <div className={styles.footer_section}>
+        <div className={styles.copyright}>© 2022 Peer, Inc.</div>
+        <div className={styles.language_wrapper}>United States (English)</div>
+      </div>
+    </div>
+  </div>
+)
 Footer.propTypes = {}
 
 export default Footer
