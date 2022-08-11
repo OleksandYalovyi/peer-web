03ff81a87882bf0eea7fa5e895d988634b8f4bbf
Alexey Lazarev <chisw.dnepr@gmail.com>
2022-08-11 18:30:13 +0300

    feature/new-home-elements, companies

---
+++ src/pages/About/components/Epic/VideoSection/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactPlayer from 'react-player/youtube'
+
+import styles from './style.module.scss'
+
+function VideoSection() {
+  return (
+    <section className={styles.section}>
+      <div className={styles.container}>
+        <div className={styles['video-wrapper']}>
+          <ReactPlayer url="https://youtu.be/CrDoVth4mLA" playing width="100%" height="100%" />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default VideoSection
---
+++ src/pages/About/components/Epic/index.js
@@ -0,0 +1,36 @@
+import React, { useCallback, useRef } from 'react'
+import MainContainer from 'components/MainContainer'
+import Arrow from 'assets/arrow-black.svg'
+import Modal from 'components/Modal'
+import Animated from 'components/Animated'
+import PropTypes from 'prop-types'
+import AsSeen from './AsSeen/index'
+import Video from './VideoSection'
+import styles from './styles.module.scss'
+
+const Epic = () => {
+  const modal = useRef(null)
+
+  const onClick = useCallback(() => modal.current.open(true), [modal.current])
+
+  return (
+    <MainContainer>
+      <Animated>
+        <div className={styles.title}>An epic, beginning.</div>
+      </Animated>
+      <Animated>
+        <div className={styles.video_link} onClick={onClick}>
+          Watch the video
+          <img className={styles.arrow} src={Arrow} alt="arrow" />
+        </div>
+      </Animated>
+      <Modal ref={modal}>
+        <Video />
+      </Modal>
+    </MainContainer>
+  )
+}
+
+Epic.propTypes = {}
+
+export default Epic
