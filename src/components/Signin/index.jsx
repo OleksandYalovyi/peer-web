cc32e6322298711a3723e7cf79ecabdbea7b9549
Andrii Zakharchenko <art.pruttov@gmail.com>
2022-08-18 11:53:46 +0300

    feature: add modal

---
+++ src/components/Signin/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from './styles.module.scss'
+
+const SignIn = () => (
+  <div className={styles.container}>
+    <div className={styles.title}>Sign in to Peer</div>
+    <form>
+      <div className={styles.input_section}>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+      </div>
+      <div className={styles.link_wrapper}>
+        <Link className={styles.forgot_pass} to="/">
+          Forgot password?
+        </Link>
+      </div>
+      <input className={styles.button} type="submit" value="Continue" />
+    </form>
+    <div className={styles.subtitle}>
+      Don’t have an account? <Link to="/">Click here</Link> to sign up.
+    </div>
+  </div>
+)
+
+export default SignIn
