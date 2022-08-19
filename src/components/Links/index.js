b9dd559f040493f29fae2ce34fae820aab67c018
Alexey Lazarev <chisw.dnepr@gmail.com>
2022-08-19 17:30:15 +0300

    feature/privacy, privacy

--- src/components/Links/index.js
+++ src/components/Links/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef } from 'react'
 import PropTypes from 'prop-types'
 import cls from 'classnames'
+import Privacy from 'assets/Privacy_Policy_Peer_Inc.pdf'
 import styles from './styles.module.scss'
 import LangSelector from './LangSelector/index'
 
@@ -15,7 +16,9 @@ const Links = () => {
       </div>
       <div className={styles.block}>
         <div className={styles.link}>Terms of Service</div>
-        <div className={styles.link}>Privacy Policy</div>
+        <a href={Privacy} className={styles.link} target="_blank" rel="noreferrer">
+          Privacy Policy
+        </a>
         <div className={styles.link}>Cookie Policy</div>
       </div>
     </div>
