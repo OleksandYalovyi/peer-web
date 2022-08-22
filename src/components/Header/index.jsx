833a5eabde5b07432eb6f6ff965d89fb442f07bf
Andrii Zakharchenko <art.pruttov@gmail.com>
2022-08-22 08:57:02 +0300

    fix: styles

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,7 +5,7 @@ import cls from 'classnames'
 import useScroll from 'hooks/useScroll'
 import PropTypes from 'prop-types'
 import Modal from 'components/ModalForm'
-import SignIn from 'components/Signin'
+import SignIn from 'components/SignIn'
 
 import styles from './styles.module.scss'
 
