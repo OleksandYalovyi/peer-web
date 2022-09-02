84473054d4725247b2a7cb3c05bf6f1d27195fb4
Alexey Lazarev <chisw.dnepr@gmail.com>
2022-09-02 14:35:01 +0300

    feature/update-page, update page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom'
 import Home from 'pages/HomeV2'
 import WhitePaper from 'pages/WhitePaper'
+import Updates from 'pages/Updates'
 import Header from 'components/Header'
 
 import styles from './app.module.scss'
@@ -14,6 +15,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/white-paper" component={WhitePaper} />
+          <Route exact path="/updates" component={Updates} />
           <Redirect exact from="/*" to="/" />
         </Switch>
       </main>
