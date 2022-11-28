1f7271d24e1f7642e8f55ab332ab628491de2bf9
Andrii Zakharchenko <art.pruttov@gmail.com>
2022-11-28 02:08:57 +0200

    fix: path for zen

---
+++ src/pages/Zen/components/Features/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './styles.module.scss'
+
+const Features = () => (
+  <section className={styles.section}>
+    <div className={styles.container}>
+      {/* <div className={styles.col_1}>Zen Features</div>
+      <div className={styles.col_2}>Find fun now</div> */}
+    </div>
+  </section>
+)
+
+export default Features
