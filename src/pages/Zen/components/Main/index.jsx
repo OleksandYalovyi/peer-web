50b7b44c42e95f38336c5fcae9553bc537c1507e
Andrii Haidai <andrii.haidai@chisw.com>
2022-12-01 15:48:46 +0200

    fix: desktop slider

--- src/pages/Zen/components/Main/index.jsx
+++ src/pages/Zen/components/Main/index.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react'
 import useCurrentWidth from 'hooks/useCurrentWidth'
-import ZEN from 'assets/zen.svg'
+import ZEN from 'assets/zenSircle.svg'
 import BackArrow from 'assets/backArrow.svg'
 import forwardArrow from 'assets/forwardArrow.svg'
 import ZenSlider from '../ZenSlider'
@@ -22,14 +22,14 @@ const Main = () => {
             <div className={styles.zen_logo}>
               <img src={ZEN} alt="ZEN" width={isMobile ? 50 : 90} />
             </div>
-            <div className={styles.slider_nav}>
+            {/* <div className={styles.slider_nav}>
               <div className={styles.arrow_wrapper} onClick={onPrevSlide}>
                 <img src={BackArrow} alt="prev Slide" width={30} />
               </div>
               <div className={styles.arrow_wrapper} onClick={onNextSlide}>
                 <img src={forwardArrow} alt="next Slide" width={30} />
               </div>
-            </div>
+            </div> */}
           </div>
           <div className={styles.title}>
             Zen is a visual ledger of global activities and events across time.
@@ -60,7 +60,7 @@ const Main = () => {
           <div className={styles.mobile_slider_nav} onClick={onPrevSlide}>
             <img src={BackArrow} alt="prev Slide" width={13.66} />
           </div>
-          <ZenSlider sliderRef={sliderRef} onNextSlide={onNextSlide} />
+          <ZenSlider sliderRef={sliderRef} onNextSlide={onNextSlide} onPrevSlide={onPrevSlide} />
         </div>
       </div>
     </section>
