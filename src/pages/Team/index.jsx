146b18b39e29bbaf5eb9b4b96518bb7a7b58540b
Andrii Zakharchenko <art.pruttov@gmail.com>
2023-01-06 10:40:06 +0200

    feature: add styles for mobile

--- src/pages/Team/index.jsx
+++ src/pages/Team/index.jsx
@@ -1,16 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import Main from './components/Main'
-import Tony from './components/Tony/index'
+import Tony from './components/Tony'
 import Creators from './components/Creators'
 import Projects from './components/Projects'
 import styles from './team.module.scss'
-import The from './components/The/index'
 
 const Team = () => (
   <section className={styles.page_container}>
-    <The />
-    <div className={styles.separator} />
     <Main />
     <div className={styles.separator} />
     <Tony />
@@ -18,7 +15,6 @@ const Team = () => (
     <Creators />
     <div className={styles.separator} />
     <Projects />
-    <div className={styles.separator} />
   </section>
 )
 
