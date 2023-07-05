4a903195cc1b7897e79c54350c028356451d5895
Andrii Haidai <andrii.haidai@chisw.com>
2023-07-05 14:41:20 +0300

    fix: mobile logo & header

--- src/components/HomeHeader/index.jsx
+++ src/components/HomeHeader/index.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { NavLink, Link } from 'react-router-dom'
 import cls from 'classnames'
 import MobileMenuLogo from 'components/Icons/MobileMenuLogo/index'
@@ -7,15 +7,18 @@ import useScroll from 'hooks/useScroll'
 import useCurrentWidth from 'hooks/useCurrentWidth'
 import Plus from 'assets/Home/plus.svg'
 
-import MobileMenu from '../Header/MobileMenu'
+// import MobileMenu from '../Header/MobileMenu'
 
 import styles from './header.module.scss'
 
-const NavItems = ({ name, links, link }) => {
+const NavItems = ({ name, links, link, openedMenu, clickHandler }) => {
   const [hovered, setHovered] = useState('')
 
   return (
-    <div className={styles.nav_item}>
+    <div
+      className={cls(styles.nav_item, { [styles.open]: openedMenu === name })}
+      onClick={() => clickHandler(name)}
+    >
       {link ? <NavLink to={link}>{name}</NavLink> : name}
       {links && (
         <div className={styles.nav_list}>
@@ -62,6 +65,24 @@ const HomeHeader = () => {
   const width = useCurrentWidth()
   const { scrollDirection } = useScroll()
   const [isMenu, setIsMenuOpen] = useState(false)
+  const [openedMenu, setIsOpen] = useState('')
+  const ref = useRef('')
+
+  const handleClick = (e) => {
+    if (ref.current && !ref.current.contains(e.target)) {
+      setIsOpen('')
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('click', handleClick)
+
+    return () => {
+      document.removeEventListener('click', handleClick)
+    }
+  })
+
+  const clickHandler = (itemName) => (openedMenu === itemName ? setIsOpen('') : setIsOpen(itemName))
 
   const isMobile = width < 681
   return (
@@ -79,68 +100,73 @@ const HomeHeader = () => {
           [styles.bg]: scrollDirection === 'up' || scrollDirection === 'bottom',
         },
       )}
+      ref={ref}
     >
       <div className={styles.main_container}>
-        <div className={styles.left_container}>
-          <Link className={styles.logo_container} to="/">
-            <img src={Plus} alt="Plus" width={25} />
-          </Link>
-        </div>
-
         {!isMobile && (
-          <nav className={styles.nav}>
-            <NavItems name="HOME" link="/" />
-            <NavItems
-              router
-              name="ABOUT"
-              links={[
-                {
-                  name: 'Team',
-                  to: '/team',
-                  router: true,
-                },
-                {
-                  name: 'Brand Book',
-                  to: '/brandbook',
-                  router: true,
-                },
-                {
-                  name: 'White Papers',
-                  to: '/whitepapers',
-                  router: true,
-                },
-              ]}
-            />
-
-            <NavItems
-              name="PRODUCTS"
-              links={[
-                { name: 'PEER', desc: 'OPEN WORLD', to: 'https://peerclub.com', logo: 'PEER' },
-                { name: 'ZEN', desc: 'WALLET', to: '/placeholder', logo: 'ZEN', router: true },
-                { name: 'OMNI', desc: 'EXPLORER', to: 'https://peermultichain.com', logo: 'OMNI' },
-                {
-                  name: 'PM',
-                  desc: 'MARKETPLACE',
-                  to: 'https://market.peerclub.com',
-                  logo: 'PM',
-                },
-                {
-                  name: 'PEER DOLLAR',
-                  desc: 'COIN',
-                  to: 'https://market.peerclub.com',
-                  logo: 'COIN',
-                },
-                // { name: 'Index', to: '/placeholder', logo: 'INDEX', router: true },
-                // { name: 'Labs', to: 'https://labs.peer.inc', logo: 'LABS' },
-              ]}
-            />
-
-            <NavItems name="PRESS" link="/" />
-          </nav>
+          <div className={styles.left_container}>
+            <Link className={styles.logo_container} to="/">
+              <img src={Plus} alt="Plus" width={25} />
+            </Link>
+          </div>
         )}
-        <div className={styles.right_container}>
-          {isMobile && <MobileMenu isMenu={isMenu} setIsMenuOpen={setIsMenuOpen} />}
-        </div>
+        <nav className={styles.nav}>
+          <NavItems name="HOME" link="/" openedMenu={openedMenu} clickHandler={clickHandler} />
+          <NavItems
+            router
+            name="ABOUT"
+            links={[
+              {
+                name: 'Team',
+                to: '/team',
+                router: true,
+              },
+              {
+                name: 'Brand Book',
+                to: '/brandbook',
+                router: true,
+              },
+              {
+                name: 'White Papers',
+                to: '/whitepapers',
+                router: true,
+              },
+            ]}
+            openedMenu={openedMenu}
+            clickHandler={clickHandler}
+          />
+
+          <NavItems
+            name="PRODUCTS"
+            links={[
+              { name: 'PEER', desc: 'OPEN WORLD', to: 'https://peerclub.com', logo: 'PEER' },
+              { name: 'ZEN', desc: 'WALLET', to: '/placeholder', logo: 'ZEN', router: true },
+              { name: 'OMNI', desc: 'EXPLORER', to: 'https://peermultichain.com', logo: 'OMNI' },
+              {
+                name: 'PM',
+                desc: 'MARKETPLACE',
+                to: 'https://market.peerclub.com',
+                logo: 'PM',
+              },
+              {
+                name: 'PEER DOLLAR',
+                desc: 'COIN',
+                to: 'https://market.peerclub.com',
+                logo: 'COIN',
+              },
+              // { name: 'Index', to: '/placeholder', logo: 'INDEX', router: true },
+              // { name: 'Labs', to: 'https://labs.peer.inc', logo: 'LABS' },
+            ]}
+            openedMenu={openedMenu}
+            clickHandler={clickHandler}
+          />
+
+          <NavItems name="PRESS" link="/" openedMenu={openedMenu} clickHandler={clickHandler} />
+        </nav>
+
+        {/* <div className={styles.right_container}> */}
+        {/* {isMobile && <MobileMenu isMenu={isMenu} setIsMenuOpen={setIsMenuOpen} />} */}
+        {/* </div> */}
       </div>
     </header>
   )
--- src/pages/Home/components/Global/index.jsx
+++ src/pages/Home/components/Global/index.jsx
@@ -7,6 +7,7 @@ import styles from './styles.module.scss'
 
 const Global = () => {
   const width = useCurrentWidth()
+  const isMobile = width < 900
 
   useEffect(() => {
     animatedImage(window)
@@ -14,22 +15,20 @@ const Global = () => {
 
   return (
     <section className={styles.global}>
-      <NavLink to="/" className={styles.link}>
-        <div className={styles.link}>
-          <img
-            src={PeerLogoGlobal}
-            alt="Peer"
-            className="next-particle"
-            data-width={width}
-            data-height="800"
-            data-color="#ffffff"
-            data-particle-gap="2"
-            data-gravity="0.1"
-            data-min-width="450"
-            data-min-height="450"
-          />
-        </div>
-      </NavLink>
+      <div className={styles.link}>
+        <img
+          src={PeerLogoGlobal}
+          alt="Peer"
+          className="next-particle"
+          data-width={width}
+          data-height={isMobile ? 400 : 800}
+          data-color="#ffffff"
+          data-particle-gap="2"
+          data-gravity="0.1"
+          data-min-width={isMobile ? 250 : 450}
+          data-min-height={isMobile ? 250 : 450}
+        />
+      </div>
     </section>
   )
 }
