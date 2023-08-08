6683e436fd3637b3ee35b4dc58aeba831626e20e
Andrii Zakharchenko <art.pruttov@gmail.com>
2023-08-08 19:38:27 +0300

    fix: placeholder

--- src/pages/Placeholder/index.jsx
+++ src/pages/Placeholder/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './placeholder.module.scss'
 
-const Placeholder = () => <div className={styles.container}>Coming Q1 2023</div>
+const Placeholder = () => <div className={styles.container}>Coming Q4 2023</div>
 
 export default Placeholder
