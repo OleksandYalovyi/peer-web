44dcf0563b1409989b2f25debfe0adeba5c14bc1
Andrew Positko <positko.andrei@gmail.com>
2023-09-27 15:25:01 +0300

    feature: INC-13, add routes for footer links

--- src/components/HomeFooter/constants.js
+++ src/components/HomeFooter/constants.js
@@ -1,62 +1,200 @@
-const FOOTER_LIST_ITEMS = [
+export const FOOTER_LIST_ITEMS = [
   {
     title: 'Platforms',
     content: [
-      'Peer social app',
-      'Peer Market',
-      'Omni block explorer',
-      'Peer Blockchain',
-      'Zen wallet',
-      'Peer Labs',
+      {
+        title: 'Peer social app',
+        to: 'https://apps.apple.com/us/app/peer/id1669571704',
+        router: false,
+      },
+      {
+        title: 'Peer Market',
+        to: 'https://market.peerclub.com/',
+        router: false,
+      },
+      {
+        title: 'Omni block explorer',
+        to: '/placeholder',
+        router: true,
+      },
+      {
+        title: 'Peer Blockchain',
+        to: '/placeholder',
+        router: true,
+      },
+      {
+        title: 'Zen wallet',
+        to: '/placeholder',
+        router: true,
+      },
+      {
+        title: 'Peer Labs',
+        to: '/placeholder',
+        router: true,
+      },
     ],
   },
   {
     title: 'Peer Inc.',
     content: [
-      'About the company',
-      'Company news',
-      'Brand toolkit',
-      'Jobs and internships',
-      'Investors',
+      {
+        title: 'About the company',
+        to: '/',
+        router: true,
+      },
+      {
+        title: 'Company news',
+        to: '/news',
+        router: true,
+      },
+      {
+        title: 'Brand toolkit',
+        to: '/placeholder',
+        router: true,
+      },
+      {
+        title: 'Jobs and internships',
+        to: '/placeholder',
+        router: true,
+      },
+      {
+        title: 'Investors',
+        to: '/placeholder',
+        router: true,
+      },
     ],
   },
   {
     title: 'Help',
     content: [
-      'Help Center',
-      'Using Peer',
-      'Peer for creators',
-      'Ads Help Center',
-      'Managing your account',
-      'Email Preference Center',
-      'Rules and policies',
-      'Contact us',
+      {
+        title: 'Help Center',
+        to: 'https://community.peerclub.com/hc/en-us',
+        router: false,
+      },
+      {
+        title: 'Using Peer',
+        to: '/placeholder',
+        router: true,
+      },
+      {
+        title: 'Peer for creators',
+        to: '/placeholder',
+        router: true,
+      },
+      {
+        title: 'Ads Help Center',
+        to: '/placeholder',
+        router: true,
+      },
+      {
+        title: 'Managing your account',
+        to: '/placeholder',
+        router: true,
+      },
+      {
+        title: 'Email Preference Center',
+        to: '/placeholder',
+        router: true,
+      },
+      {
+        title: 'Rules and policies',
+        to: 'https://community.peerclub.com/hc/en-us',
+        router: false,
+      },
+      {
+        title: 'Contact us',
+        to: 'https://community.peerclub.com/hc/en-us/requests/new',
+        router: false,
+      },
     ],
   },
   {
     title: 'Developer resources',
     content: [
-      'Developer home',
-      'Documentation',
-      'Forums Communities',
-      'Developer blog',
-      'Engineering blog',
-      'Developer terms',
+      {
+        title: 'Developer home',
+        to: '/placeholder',
+        router: true,
+      },
+      {
+        title: 'Documentation',
+        to: '/placeholder',
+        router: true,
+      },
+      {
+        title: 'Forums Communities',
+        to: '/placeholder',
+        router: true,
+      },
+      {
+        title: 'Developer blog',
+        to: '/placeholder',
+        router: true,
+      },
+      {
+        title: 'Engineering blog',
+        to: '/placeholder',
+        router: true,
+      },
+      {
+        title: 'Developer terms',
+        to: '/placeholder',
+        router: true,
+      },
     ],
   },
   {
     title: 'Business resources',
     content: [
-      'Advertise',
-      'Peer for business',
-      'Resources and guides',
-      'Peer for marketers',
-      'Marketing insights',
-      'Brand inspiration',
+      {
+        title: 'Advertise',
+        to: '/placeholder',
+        router: true,
+      },
+      {
+        title: 'Peer for business',
+        to: '/placeholder',
+        router: true,
+      },
+      {
+        title: 'Resources and guides',
+        to: '/placeholder',
+        router: true,
+      },
+      {
+        title: 'Peer for marketers',
+        to: '/placeholder',
+        router: true,
+      },
+      {
+        title: 'Marketing insights',
+        to: '/placeholder',
+        router: true,
+      },
+      {
+        title: 'Brand inspiration',
+        to: '/placeholder',
+        router: true,
+      },
     ],
   },
 ]
 
-export default {
-  FOOTER_LIST_ITEMS,
-}
+export const BOTTOM_LINKS = [
+  {
+    title: 'Cookies',
+    to: '/placeholder',
+    router: true,
+  },
+  {
+    title: 'Privacy',
+    to: 'https://community.peerclub.com/hc/en-us/articles/17261918037787-privacy-policy',
+    router: false,
+  },
+  {
+    title: 'Terms and conditions',
+    to: 'https://community.peerclub.com/hc/en-us/articles/17262293261979-terms-of-service',
+    router: false,
+  },
+]
