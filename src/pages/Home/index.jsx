af7cf4991c616885d13b1146998845e82d657ad2
Andrew Positko <positko.andrei@gmail.com>
2023-11-14 16:43:03 +0200

    feature [WEB-197]: create Home with styles

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,53 +2,18 @@
 import React, { useCallback, useState } from 'react'
 
 import styles from './home.module.scss'
-// import Global from './components/Global'
-// import Menu from './components/Menu'
-import News from './components/News'
-import Peer from './components/Peer'
-// import Puzzle from './components/Puzzle'
-import MetaEarth from './components/MetaEarh'
-import Involved from './components/Involved'
-import Dates from './components/Dates'
-import Vision from './components/Vision/index'
-import Manifesto from './components/Manifesto/index'
+import PeerMain from './components/PeerMain/index'
+import HomeHeader from 'components/HomeHeader/index'
+import PeerServices from './components/PeerServices/index'
 
 const Home = () => {
-  const [userPosition, setUserPosition] = useState({
-    x: 0,
-    y: 0,
-  })
-
-  const onMouseMove = useCallback((e) => {
-    setUserPosition({
-      x: e.clientX,
-      y: e.clientY,
-    })
-  }, [])
 
   return (
-    <div className={styles.wrapper} onMouseMove={onMouseMove}>
-      {/* <div className={styles.container}>
-        <Global />
-      </div> */}
-
-      {/* <Menu /> */}
-
-      <News />
-
-      <Dates userPosition={userPosition} />
-
-      <div className={styles.container}>
-        <Peer />
-      </div>
-
-      <MetaEarth />
-
-      <div className={styles.container}>
-        {/* <Puzzle /> */}
-        <Vision />
-        <Manifesto />
-        <Involved />
+    <div className={styles.wrapper}>
+      <HomeHeader /> 
+      <PeerMain />
+      <PeerServices />
+      <div className={styles.container}>  
       </div>
     </div>
   )
