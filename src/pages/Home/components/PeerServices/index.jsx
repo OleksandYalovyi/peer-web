6960cb0f9eec2faafedd23d27b190cd7f6541b36
Andrew Positko <positko.andrei@gmail.com>
2023-11-17 09:46:03 +0200

    feature [WEB-227] add styles for resp for icons

--- src/pages/Home/components/PeerServices/index.jsx
+++ src/pages/Home/components/PeerServices/index.jsx
@@ -3,25 +3,31 @@ import Peer from 'components/Icons/PeerServices/Peer'
 import Playa from 'components/Icons/PeerServices/Playa'
 import Vine from 'components/Icons/PeerServices/Vine'
 import Zen from 'components/Icons/PeerServices/Zen'
+import useCurrentWidth from 'hooks/useCurrentWidth'
 
 import styles from './peerservices.module.scss'
 
-const PeerServices = () => (
-  <section className={styles.section}>
-    <div className={styles.container}>
-      <a
-        href="https://apps.apple.com/us/app/peer/id1669571704"
-        target="_blank"
-        rel="noreferrer"
-        className={styles.logo_link}
-      >
-        <Peer />
-      </a>
-      <Playa />
-      <Vine />
-      <Zen />
-    </div>
-  </section>
-)
+const PeerServices = () => {
+  const width = useCurrentWidth()
+  const isSmallScreen = width < 1160
+
+  return (
+    <section className={styles.section}>
+      <div className={styles.container}>
+        <a
+          href="https://apps.apple.com/us/app/peer/id1669571704"
+          target="_blank"
+          rel="noreferrer"
+          className={styles.logo_link}
+        >
+          <Peer width={isSmallScreen ? '120' : null} />
+        </a>
+        <Playa width={isSmallScreen ? '150' : null} />
+        <Vine width={isSmallScreen ? '100' : null} />
+        <Zen width={isSmallScreen ? '140' : null} />
+      </div>
+    </section>
+  )
+}
 
 export default PeerServices
