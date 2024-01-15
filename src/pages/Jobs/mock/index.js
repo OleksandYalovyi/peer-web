b87faa625b7630d9d86adabf60daf5d3487b9b4c
Andrew Positko <positko.andrei@gmail.com>
2024-01-15 09:21:40 +0200

    [WEB-737]: create mocked data

---
+++ src/pages/Jobs/mock/index.js
@@ -0,0 +1,28 @@
+import ProductManagerProductDesign from './pdf/ProductManager_ProductDesign.pdf'
+
+export const jobData = [
+  {
+    id: 1,
+    title: 'Product Designer',
+    location: 'Remote',
+    time: 'Full Time',
+    linkFile: ProductManagerProductDesign,
+    link: 'mailto:jobs@peer.inc',
+  },
+  {
+    id: 2,
+    title: 'Head of Design',
+    location: 'Remote',
+    time: 'Full Time',
+    linkFile: ProductManagerProductDesign,
+    link: 'mailto:jobs@peer.inc',
+  },
+]
+
+export const defaultJobData = [
+  {
+    title: 'Open Application',
+    location: 'Remote',
+    link: 'mailto:jobs@peer.inc',
+  },
+]
