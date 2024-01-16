10a29e5c92ae597f40198f9d380e9fb4846fd927
Andrew Positko <positko.andrei@gmail.com>
2024-01-16 09:54:51 +0200

    [feature/WEB-737]: fix errors

--- src/pages/Jobs/Item/icons.jsx
+++ src/pages/Jobs/Item/icons.jsx
@@ -1,3 +1,7 @@
+/* eslint-disable max-len */
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable import/prefer-default-export */
+
 export function IconLocation() {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 26 26" fill="none">
--- src/pages/Jobs/index.jsx
+++ src/pages/Jobs/index.jsx
@@ -6,60 +6,64 @@ import styles from './styles.module.scss'
 import { defaultJobData, jobData } from './mock/index'
 
 const buttons = [
-	'all',
-	'product & design',
-	'hr & finances',
-	'data',
-	'marketing',
-	'engineering',
-	'other',
+  'all',
+  'product & design',
+  'hr & finances',
+  'data',
+  'marketing',
+  'engineering',
+  'other',
 ]
 
 function Jobs() {
-	const [selectedItem, setSelectedItem] = useState(null);
+  const [selectedItem, setSelectedItem] = useState(null)
 
-	function capitalizeFirstLetter(str) {
-		return str.replace(/\b\w/g, item => item.toUpperCase());
-	}
-	
-	const handleBtnClick = (itemName) => {
-    setSelectedItem(capitalizeFirstLetter(itemName));
-  };
-	return (
-		<div className={styles.container}>
-			<section className={styles.header}>
-				<div className={styles.description}>
-					<h1 className={styles.title}>Go Team Go!</h1>
-					<p className={styles.text}>
-						Built by a small, scrappy team of experienced builders with a strong background in consumer products and a relentless focus on the intersection of culture and community.
-					</p>
-					<p className={styles.text}>
-						Our mission is to create a the ultimate hotspot for people that LIVES to play, learn, work and hang together.
-					</p>
-				</div>
-				<div className={styles.stars}>
-					<img src={Stars} alt="stars" />
-				</div>
-				<div className={styles.play}>
-					<img src={Play} alt="play" />
-				</div>
-			</section>
-			<section>
-				<ul className={styles.button_group}>
-					{buttons.map((btn) => (
-						<li key={btn} onClick={() => handleBtnClick(btn)}>{btn}</li>
-					))}
-				</ul>
-				<h2 className={styles.jobs_title}>{selectedItem || `No positions open`}</h2>
-			
-				<div className={styles.jobs_list}>
-					{[...jobData, ...defaultJobData].map((item) => (
-						<CareersItem {...item} />
-					))}
-				</div>
-			</section>
-		</div>
-	)
+  function capitalizeFirstLetter(str) {
+    return str.replace(/\b\w/g, (item) => item.toUpperCase())
+  }
+
+  const handleBtnClick = (itemName) => {
+    setSelectedItem(capitalizeFirstLetter(itemName))
+  }
+  return (
+    <div className={styles.container}>
+      <section className={styles.header}>
+        <div className={styles.description}>
+          <h1 className={styles.title}>Go Team Go!</h1>
+          <p className={styles.text}>
+            Built by a small, scrappy team of experienced builders with a strong background in
+            consumer products and a relentless focus on the intersection of culture and community.
+          </p>
+          <p className={styles.text}>
+            Our mission is to create a the ultimate hotspot for people that LIVES to play, learn,
+            work and hang together.
+          </p>
+        </div>
+        <div className={styles.stars}>
+          <img src={Stars} alt="stars" />
+        </div>
+        <div className={styles.play}>
+          <img src={Play} alt="play" />
+        </div>
+      </section>
+      <section>
+        <ul className={styles.button_group}>
+          {buttons.map((btn) => (
+            <li key={btn} onClick={() => handleBtnClick(btn)}>
+              {btn}
+            </li>
+          ))}
+        </ul>
+        <h2 className={styles.jobs_title}>{selectedItem || `No positions open`}</h2>
+
+        <div className={styles.jobs_list}>
+          {[...jobData, ...defaultJobData].map((item) => (
+            <CareersItem {...item} />
+          ))}
+        </div>
+      </section>
+    </div>
+  )
 }
 
 export default Jobs
