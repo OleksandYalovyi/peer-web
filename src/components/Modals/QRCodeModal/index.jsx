5827fa4a54b497448de6883672a008d6ba63cae5
Andrii Zakharchenko <AndrZakhara@users.noreply.github.com>
2024-01-31 09:55:11 +0200

    refactor[web-751]: update styles for modal
    * fix[web-751]: get link redirects for mobile
    
    * refactor[web-751]: update styles

--- src/components/Modals/QRCodeModal/index.jsx
+++ src/components/Modals/QRCodeModal/index.jsx
@@ -3,6 +3,9 @@ import T from 'prop-types'
 import QRCode from 'react-qr-code'
 import Modal from 'components/Modal'
 import { IconCLose } from 'components/Icons/Close'
+import links from 'constants/links'
+import appstore from 'assets/qr/app-store.svg'
+import google from 'assets/qr/google.svg'
 import styles from './qr_code_modal.module.scss'
 
 function QRCodeModal({ isOpen, onClose }) {
@@ -15,6 +18,7 @@ function QRCodeModal({ isOpen, onClose }) {
   return (
     <Modal open={isOpen} onClose={onClose} className={styles.qr_code_wrapper}>
       <div className={styles.container}>
+        <div className={styles.description}>Scan to download</div>
         <button className={styles.close} type="button" onClick={onClose}>
           <IconCLose />
         </button>
@@ -23,9 +27,17 @@ function QRCodeModal({ isOpen, onClose }) {
             style={{ height: 'auto', maxWidth: '100%', width: '100%' }}
             value={url}
             viewBox="0 0 256 256"
+            bgColor="rgba(255, 255, 255, 0)"
           />
         </div>
-        <div className={styles.description}>Scan to download</div>
+        <div className={styles.store_links_container}>
+          <a href={links.appStore} className={styles.store_link}>
+            <img src={appstore} alt="app store" />
+          </a>
+          <a href={links.googlePlay} className={styles.store_link}>
+            <img src={google} alt="google play" />
+          </a>
+        </div>
       </div>
     </Modal>
   )
