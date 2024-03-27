847003ece66726e067453cee3e9b354c3e6bf40d
MeleshkoAleksander <131786212+MeleshkoAleksander@users.noreply.github.com>
2024-03-27 17:42:42 +0200

    disabled unused pages (#520)

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ import Home from 'pages/Home'
 import Page404 from 'pages/Page404/index'
 import MainHeader from 'components/Header/MainHeader'
 import SimpleHeader from 'components/Header/SimpleHeader'
-import Zen from 'pages/Zen'
-import About from 'pages/About'
-import Placeholder from 'pages/Placeholder'
+// import Zen from 'pages/Zen'
+// import About from 'pages/About'
+// import Placeholder from 'pages/Placeholder'
 import Jobs from 'pages/Jobs'
-import OSDetect from 'pages/OSDetect'
+// import OSDetect from 'pages/OSDetect'
 import Blog from 'pages/Blog'
 import Article from 'pages/Article'
 import Team from 'pages/Team'
@@ -41,16 +41,15 @@ function App() {
       <TransitionGroup className="transition_group">
         <CSSTransition key={key} classNames="fade" timeout={900}>
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/zen" element={<Zen />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/placeholder" element={<Placeholder />} />
+            <Route path={routing.withoutAuth.home} element={<Home />} />
+            {/* <Route path="/zen" element={<Zen />} /> */}
+            {/* <Route path="/about" element={<About />} /> */}
+            {/* <Route path="/placeholder" element={<Placeholder />} /> */}
             <Route path={routing.withoutAuth.blog} element={<Blog />} />
             <Route path={`${routing.withoutAuth.blog}/:id?`} element={<Article />} />
             <Route path={routing.withoutAuth.team} element={<Team />} />
             <Route path={routing.withoutAuth.jobs} element={<Jobs />} />
-            <Route path={routing.withoutAuth.team} element={<Team />} />
-            <Route path={routing.withoutAuth.app} element={<OSDetect />} />
+            {/* <Route path={routing.withoutAuth.app} element={<OSDetect />} /> */}
             <Route path={routing.withoutAuth.notFound} element={<Page404 />} />
             <Route path="/*" element={<Navigate to={routing.withoutAuth.notFound} />} />
           </Routes>
