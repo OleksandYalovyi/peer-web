728e9670f3130672d4a3f0822001897ee15ba48d
TilikWebDeveloper <tilikwebdeveloper@gmail.com>
2024-08-20 02:23:41 +0300

    fix [WEB-1807]: Added Image Preload

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/media-has-caption */
-import React from 'react'
+import React, { useState } from 'react'
+import SolarPreloadImg from 'assets/Home/SolarPreload.png'
 import ArrowLeftRounded from 'assets/Home/arrowLeftRounded.svg'
 import LittleLeftChevron from 'assets/Home/littleLeftChevron.svg'
 import Solar from 'assets/Home/video/solar.webm'
@@ -7,14 +8,31 @@ import Title from './components/Title'
 import styles from './home.module.scss'
 
 function Home() {
+  const [isVideoLoaded, setIsVideoLoaded] = useState(false)
+
+  const handleVideoLoaded = () => {
+    setIsVideoLoaded(true)
+  }
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
         <div className={styles.content}>
           <Title />
+
           <div className={styles.video}>
-            <video src={Solar} autoPlay loop muted />
+            <video
+              src={Solar}
+              autoPlay
+              loop
+              muted
+              onLoadedData={handleVideoLoaded}
+              style={{ display: isVideoLoaded ? 'block' : 'none' }}
+            />
+
+            {!isVideoLoaded && <img src={SolarPreloadImg} alt="Solar preload" />}
           </div>
+
           <div className={styles.footer}>
             <a className={styles.largeLink}>
               Our journey with Peer
