90be7dd3d2301396f77fe80519e846f6bba00e22
Oleksandr Yalovyi <110346303+OleksandYalovyi@users.noreply.github.com>
2024-09-04 09:18:10 +0200

    Fix[WEB-1848]: add home responsive for 2k (#586)
    * fix[Web-1848]: fix header for 2k
    
    * fix[WEB-1848]: add 2k responsive for home page

--- src/components/Header/MainHeader/index.jsx
+++ src/components/Header/MainHeader/index.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
-import React, { useState, useEffect, useRef } from 'react'
-import { NavLink, Link, useLocation } from 'react-router-dom'
+import React, { useRef } from 'react'
+import { Link, useLocation } from 'react-router-dom'
 import cls from 'classnames'
 import useScroll from 'hooks/useScroll'
 import peerLogo from 'assets/PeerLogo.svg'
@@ -9,48 +9,17 @@ import QRCodeModal from 'components/Modals/QRCodeModal'
 import useQRCodeModal from 'components/Modals/QRCodeModal/useQRCodeModal'
 import routing from 'routing/path'
 import GetButton from 'components/GetButton'
-import styles from './header.module.scss'
-import useCurrentSize from '../../../hooks/useCurrentSize'
+import useCurrentSize from 'hooks/useCurrentSize'
 
-function NavItems({ name, link, openedMenu, clickHandler }) {
-  return (
-    <div
-      className={cls(styles.nav_item, { [styles.open]: openedMenu === name })}
-      onClick={() => clickHandler(name)}
-    >
-      {link ? <NavLink to={link}>{name}</NavLink> : name}
-    </div>
-  )
-}
+import styles from './header.module.scss'
 
 function MainHeader() {
   const { pathname } = useLocation()
   const { scrollDirection } = useScroll()
-  const [openedMenu, setIsOpen] = useState('')
   const { isQRCodeModalOpen, setIsQRCodeModalOpen, closeQRCodeModal } = useQRCodeModal()
   const ref = useRef('')
   const { width } = useCurrentSize()
-  const [isMobileOpen, setIsMobileOpen] = useState(false)
   const isMobile = width < 680
-
-  const handleClick = (e) => {
-    if (ref.current && !ref.current.contains(e.target)) {
-      setIsOpen('')
-    }
-  }
-
-  useEffect(() => {
-    document.addEventListener('click', handleClick)
-
-    return () => {
-      document.removeEventListener('click', handleClick)
-    }
-  })
-
-  const logoClickHandler = () => {
-    setIsOpen('')
-    setIsMobileOpen(false)
-  }
 
   return (
     <header
@@ -67,32 +36,18 @@ function MainHeader() {
       ref={ref}
     >
       <div className={styles.main_container}>
-        <div className={styles.main_container__left}>
-          <Link to="/">
-            {pathname === routing.withoutAuth.home ? (
-              <img
-                src={peerLogoSmall}
-                alt="peer logo"
-                className={styles.peerLogoSmall}
-                onClick={logoClickHandler}
-              />
-            ) : (
-              <img
-                src={peerLogo}
-                alt="peer logo"
-                className={styles.peerLogo}
-                onClick={logoClickHandler}
-              />
-            )}
-          </Link>
-        </div>
-        <div className={styles.main_container__right}>
-          {!isMobile && (
-            <div className={styles.get_peer_wrapper}>
-              <GetButton label="I’M READY" onClick={setIsQRCodeModalOpen} />
-            </div>
+        <Link to="/">
+          {pathname === routing.withoutAuth.home ? (
+            <img src={peerLogoSmall} alt="peer logo" className={styles.peerLogoSmall} />
+          ) : (
+            <img src={peerLogo} alt="peer logo" className={styles.peerLogo} />
           )}
-        </div>
+        </Link>
+        {!isMobile && (
+          <div className={styles.get_peer_wrapper}>
+            <GetButton label="I’M READY" onClick={setIsQRCodeModalOpen} />
+          </div>
+        )}
       </div>
       <QRCodeModal isOpen={isQRCodeModalOpen} onClose={closeQRCodeModal} />
     </header>
